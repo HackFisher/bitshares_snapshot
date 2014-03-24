2f9ac5be16b1eb78a7981f1f5402e9423655c435
HackFisher <hackfisher@gmail.com>
2014-03-24 15:26:28 +0800

    lotto cli and init program codes

---
+++ libraries/lotto/include/bts/lotto/lotto_cli.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <bts/cli/cli.hpp>
+#include <bts/client/client.hpp>
+#include <bts/lotto/lotto_db.hpp>
+
+namespace bts { namespace lotto {
+    using namespace client;
+    class lotto_cli : public bts::cli::cli
+    {
+        public:
+            lotto_cli( const client_ptr& c );
+            ~lotto_cli();
+            virtual void print_help();
+            virtual void process_command( const std::string& cmd, const std::string& args );
+            
+            virtual void list_transactions( uint32_t count = 0 );
+            virtual void get_balance( uint32_t min_conf, uint16_t unit = 0 );
+
+        private:
+            // std::unique_ptr<detail::cli_impl> my;
+     };
+
+} } // bts::lotto
